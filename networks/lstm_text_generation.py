001f9d1a3d70864ea95e0bc2f1ce1649d1f0a211
olirowlands <oli.rowlands@gmail.com>
2017-01-20 22:44:16 +0000

    Update lstm_text_generation.py

--- networks/lstm_text_generation.py
+++ networks/lstm_text_generation.py
@@ -20,8 +20,10 @@ import numpy as np
 import random
 import sys
 
-path = get_file('Marcus_Aurelius_Meditations.txt', origin="https://raw.githubusercontent.com/olirowlands/keras-playground/master/data/Marcus_Aurelius_Meditations.txt")
+'''path = get_file('Marcus_Aurelius_Meditations.txt', origin="https://raw.githubusercontent.com/olirowlands/keras-playground/master/data/Marcus_Aurelius_Meditations.txt")
 text = open(path).read().lower()
+'''
+text = open("data/Marcus_Aurelius_Meditations.txt").read().lower()
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
